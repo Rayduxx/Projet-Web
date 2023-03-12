6baa0d7d51f5ae323b90db8bf5004c462495d5b2
Rayduxx <moussa.ala@esprit.tn>
2023-03-12 06:18:39 +0100

    updates

--- view/index.php
+++ view/index.php
@@ -5,8 +5,16 @@ include "./includes/header.php";
 <main>
     <div class="contents">
         <div class="overlay">
-        <img src="../assets/img/logo.png" class="hlogo">
-        <h1>MIND ZONE</h1>
+            <img src="../assets/img/logo.png" class="hlogo">
+            <h1>MIND ZONE</h1>
+        </div>
+    </div>
+    <div class="contents2">
+        <div class="wellbeingtabs">
+            <img src="../assets/img/wellbeingtab.svg" alt="Wellbeing" class="wellbeingtab">
+        </div>
+        <div class="wb-contents">
+            
         </div>
     </div>
     <div class="test"></div>
