27b0e80a721d6bd5beb7c5c124e75e21c268f94e
ilyeschaabani <ilyeschaabani857@gmail.com>
2023-03-17 12:28:01 +0100

    prepare for checkbox formulair

--- view/index.php
+++ view/index.php
@@ -11,12 +11,34 @@ include "./includes/header.php";
     </div>
     <div class="contents2">
         <div class="wellbeingtabs">
-            <img src="../assets/img/wellbeingtab.svg" alt="Wellbeing" class="wellbeingtab">
+            <!-- <img src="../assets/img/wellbeingtab.svg" alt="Wellbeing" class="wellbeingtab"> -->
+            <h2>MIND ZONE</h2>
         </div>
         <div class="wb-contents">
+            <div class="step1">
+                <h2>MINDZOE is here for you , <br> what's your problems</h2>
+            </div>
+            <form methode="post">
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+                <label for="anixiety">anexity</label>
+            </form>
             
         </div>
     </div>
-    <div class="test"></div>
+    <div class="test">
+
+    </div>
 </main>
 <?php include "./includes/footer.php"; ?>
