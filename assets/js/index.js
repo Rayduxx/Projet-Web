5badeed1ba0c8e09037659dfa0646dd32e0dffaa
ilyeschaabani <ilyeschaabani857@gmail.com>
2023-03-20 08:52:34 +0100

    formulaire php preparing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const element = document.querySelector('#monelement');
+var form = document.querySelector('form');
 
 function scrollToElement() {
     // Utilise la méthode `scrollIntoView()` pour faire défiler la page vers l'élément
@@ -7,3 +8,20 @@ function scrollToElement() {
 
   const bouton = document.querySelector('#monbouton');
 bouton.addEventListener('click', scrollToElement);
+
+// formulaire
+form.addEventListener('submit', function(event) {
+  // Get the values of the checked boxes
+  var options = [];
+  var checkboxes = form.querySelectorAll('input[type=checkbox]:checked');
+  for (var i = 0; i < checkboxes.length; i++) {
+    options.push(checkboxes[i].value);
+    console.log(1);
+  }
+    // Add the values to a hidden field in the form
+    var hiddenField = document.createElement('input');
+    hiddenField.setAttribute('type', 'hidden');
+    hiddenField.setAttribute('name', 'options');
+    hiddenField.setAttribute('value', options.join(','));
+    form.appendChild(hiddenField);
+  });
--- view/index.php
+++ view/index.php
@@ -18,28 +18,28 @@ include "./includes/header.php";
             <div class="step1">
                 <h2>We take care of our guests <br> MINDZOE is here for you , <br> whats are your problems ?</h2>
             </div>
-            <form methode="post">
-                <label for="anixiety"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >anexity</label>
-                <label for="Alcohol"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >Alcohol</label>
-                <label for="addiction and recovery"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >addiction and recovery</label>
-                <label for="depression"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >depression</label>
-                <label for="drug use"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >drug use</label>
-                <label for="eating and body image"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >eating and body image</label>
-                <label for="grief and loss"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >grief and loss</label>
-                <label for="in crisis"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >in crisis</label>
-                <label for="mental health care"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >mental health care</label>
-                <label for="mental illness"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >mental illness</label>
-                <label for="relationship and family violence"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >relationship and family violence</label>
-                <label for="stress"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >stress</label>
-                <label for="support needs"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >support needs</label>
-                <label for="understanding mental health"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >understanding mental health</label>
-                <label for="understanding wellness"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >understanding wellness</label>
-                <label for="victim support"><input type="checkbox" id="anexiety" name="maCheckbox" value="valeur1" >victim support</label>
+            <form methode="POST" action="../controller/formulaire.php">
+                <label for="anixiety"><input type="checkbox" id="anexiety" name="ch[]" value="anexity" >anexity</label>
+                <label for="Alcohol"><input type="checkbox" id="anexiety" name="ch[]" value="Alcohol" >Alcohol</label>
+                <label for="addiction and recovery"><input type="checkbox" id="anexiety" name="ch[]" value="addiction and recovery" >addiction and recovery</label>
+                <label for="depression"><input type="checkbox" id="anexiety" name="ch[]" value="depression" >depression</label>
+                <label for="drug use"><input type="checkbox" id="anexiety" name="ch[]" value="drug use" >drug use</label>
+                <label for="eating and body image"><input type="checkbox" id="anexiety" name="ch[]" value="eating and body image" >eating and body image</label>
+                <label for="grief and loss"><input type="checkbox" id="anexiety" name="ch[]" value="grief and loss" >grief and loss</label>
+                <label for="in crisis"><input type="checkbox" id="anexiety" name="ch[]" value="in crisis" >in crisis</label>
+                <label for="mental health care"><input type="checkbox" id="anexiety" name="ch[]" value="mental health care" >mental health care</label>
+                <label for="mental illness"><input type="checkbox" id="anexiety" name="ch[]" value="mental illness" >mental illness</label>
+                <label for="relationship and family violence"><input type="checkbox" id="anexiety" name="ch[]" value="relationship and family violence" >relationship and family violence</label>
+                <label for="stress"><input type="checkbox" id="anexiety" name="ch[]" value="stress" >stress</label>
+                <label for="support needs"><input type="checkbox" id="anexiety" name="ch[]" value="support needs" >support needs</label>
+                <label for="understanding mental health"><input type="checkbox" id="anexiety" name="ch[]" value="understanding mental health" >understanding mental health</label>
+                <label for="understanding wellness"><input type="checkbox" id="anexiety" name="ch[]" value="understanding wellness" >understanding wellness</label>
+                <label for="victim support"><input type="checkbox" id="anexiety" name="ch[]" value="victim support" >victim support</label>
                
                 
             </form>
             <div class="btns">
-                <button>next</button>
+                <button type="submit">NEXT</button> 
                 <button>pevious</button>
                 
             </div>
