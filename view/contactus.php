ba062735c1fc321387f380c55e1bacfab8b4d8c1
Rayduxx <moussa.ala@esprit.tn>
2023-04-04 09:34:37 +0100

    updates

---
+++ view/contactus.php
@@ -0,0 +1,56 @@
+<?php
+$page_titre = "Contact Us";
+include "./includes/header.php";
+?>
+<section>
+    <h6 class="h6mtt">MEET THE TEAM</h6>
+    <div class="cont">
+    <i class="bi bi-chevron-left"></i>
+    <i src="../assets/img/chevron-left.svg"></i>
+    </div>
+<!-- <div class="container-profile">
+        <div class="card-profile">
+            <div class="card-inner">
+                <div class="frontAla">
+                    <h2>Ala Moussa</h2>
+                    <p>Founder</p>
+                </div>
+                <div class="back">
+                    <div class="pbi">
+                     <img src="../assets/img/user.jpg"> 
+                    </div>
+                    <h1>Ala <br><span>Moussa</span></h1>
+                    <p>I am a founder and web developer of Mindzone
+                    webiste at<span> AFTERMATH.</span></p>  
+                     <div class="row-profile">
+                        <button>Follow</button>
+                        <a href="#"><img src="../assets/img/instagram.png"></a>
+                        <a href="#"><img src="../assets/img/pinterest.png"></a>
+                     </div> 
+                </div>
+            </div>
+        </div>
+        <div class="card-profile">
+            <div class="card-inner">
+                <div class="frontHamza">
+                    <h2>Hamza ben Jem</h2>
+                    <p>Founder</p>
+                </div>
+                <div class="back">
+                    <div class="pbi">
+                    <img src="../assets/img/user.jpg">
+                    </div>
+                    <h1>Ala <br><span>Moussa</span></h1>
+                    <p>I am a founder and web developer of Mindzone
+                    webiste at<span> AFTERMATH.</span></p>  
+                     <div class="row-profile">
+                        <button>Follow</button>
+                        <a href="#"><img src="../assets/img/instagram.png"></a>
+                        <a href="#"><img src="../assets/img/pinterest.png"></a>
+                     </div> 
+                </div>
+            </div>
+        </div>
+    </div> -->
+</section>
+<?php include './includes/footer.php'; ?>   
--- view/includes/header.php
+++ view/includes/header.php
@@ -14,8 +14,10 @@ if($userexist == 1) {
   <meta charset="UTF-8">
   <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/navbar.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">  <link rel="stylesheet" href="../assets/css/navbar.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../assets/css/index.css">
+  <link rel="stylesheet" href="../assets/css/index2.css">
   <link rel="stylesheet" href="../assets/css/connection.css"/>
   <link rel="stylesheet" href="../assets/css/normalize.css"/>
   <title>Mindzone - <?php echo $page_titre;?></title>
@@ -42,7 +44,7 @@ if($userexist == 1) {
       <div class="navbar-links">
       <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="contactus.php">Contact Us</a></li>
         <li><a href="profil.php"><?php echo $userinfo['nom']; ?></a></li>
       </ul>
     </div>
