17890cc09abb4f5961ce1248a21139262b3c4880
ilyeschaabani <ilyeschaabani857@gmail.com>
2023-04-04 09:20:36 +0100

    formulaire preperation done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,50 @@
-const element = document.querySelector('#monelement');
-var form = document.querySelector('form');
+
 
 function scrollToElement() {
     // Utilise la méthode `scrollIntoView()` pour faire défiler la page vers l'élément
-    element.scrollIntoView({ behavior: 'smooth' });
+    form.scrollIntoView({ behavior: 'smooth' });
   }
 
   const bouton = document.querySelector('#monbouton');
 bouton.addEventListener('click', scrollToElement);
 
-// formulaire
-form.addEventListener('submit', function(event) {
-  // Get the values of the checked boxes
-  var options = [];
-  var checkboxes = form.querySelectorAll('input[type=checkbox]:checked');
-  for (var i = 0; i < checkboxes.length; i++) {
-    options.push(checkboxes[i].value);
-    console.log(1);
+// // formulaire
+// form.addEventListener('submit', function(event) {
+//   // Get the values of the checked boxes
+//   var options = [];
+//   var checkboxes = form.querySelectorAll('input[type=checkbox]:checked');
+//   for (var i = 0; i < checkboxes.length; i++) {
+//     options.push(checkboxes[i].value);
+//     console.log(1);
+//   }
+//     // Add the values to a hidden field in the form
+//     var hiddenField = document.createElement('input');
+//     hiddenField.setAttribute('type', 'hidden');
+//     hiddenField.setAttribute('name', 'options');
+//     hiddenField.setAttribute('value', options.join(','));
+//     form.appendChild(hiddenField);
+//   });
+const form = document.getElementById('question-form');
+const questions = form.querySelectorAll('.question');
+const nextBtn = document.getElementById('next-btn');
+let currentQuestion = 0;
+
+function showNextQuestion(e) {
+  e.preventDefault();
+  // Hide current question
+  questions[currentQuestion].style.opacity = '0';
+  questions[currentQuestion].style.visibility = 'hidden';
+  
+  // Show next question
+  currentQuestion++;
+  if (currentQuestion >= questions.length) {
+    //If there are no more questions, submit form
+    form.submit();
+  } else {
+    questions[currentQuestion].style.opacity = '1';
+    questions[currentQuestion].style.visibility = 'visible';
   }
-    // Add the values to a hidden field in the form
-    var hiddenField = document.createElement('input');
-    hiddenField.setAttribute('type', 'hidden');
-    hiddenField.setAttribute('name', 'options');
-    hiddenField.setAttribute('value', options.join(','));
-    form.appendChild(hiddenField);
-  });
+}
+
+// Add event listener to Next button
+nextBtn.addEventListener('click', showNextQuestion);
--- view/index.php
+++ view/index.php
@@ -14,37 +14,55 @@ include "./includes/header.php";
         </div>
     </div>
     <div class="contents2">
-        <div class="wb-contents" id ="monelement">
-            <div class="step1">
-                <h2>We take care of our guests <br> MINDZOE is here for you , <br> whats are your problems ?</h2>
-            </div>
-            <form methode="POST" action="../controller/formulaire.php">
-                <label for="anixiety"><input type="checkbox" id="anexiety" name="ch[]" value="anexity" >anexity</label>
-                <label for="Alcohol"><input type="checkbox" id="anexiety" name="ch[]" value="Alcohol" >Alcohol</label>
-                <label for="addiction and recovery"><input type="checkbox" id="anexiety" name="ch[]" value="addiction and recovery" >addiction and recovery</label>
-                <label for="depression"><input type="checkbox" id="anexiety" name="ch[]" value="depression" >depression</label>
-                <label for="drug use"><input type="checkbox" id="anexiety" name="ch[]" value="drug use" >drug use</label>
-                <label for="eating and body image"><input type="checkbox" id="anexiety" name="ch[]" value="eating and body image" >eating and body image</label>
-                <label for="grief and loss"><input type="checkbox" id="anexiety" name="ch[]" value="grief and loss" >grief and loss</label>
-                <label for="in crisis"><input type="checkbox" id="anexiety" name="ch[]" value="in crisis" >in crisis</label>
-                <label for="mental health care"><input type="checkbox" id="anexiety" name="ch[]" value="mental health care" >mental health care</label>
-                <label for="mental illness"><input type="checkbox" id="anexiety" name="ch[]" value="mental illness" >mental illness</label>
-                <label for="relationship and family violence"><input type="checkbox" id="anexiety" name="ch[]" value="relationship and family violence" >relationship and family violence</label>
-                <label for="stress"><input type="checkbox" id="anexiety" name="ch[]" value="stress" >stress</label>
-                <label for="support needs"><input type="checkbox" id="anexiety" name="ch[]" value="support needs" >support needs</label>
-                <label for="understanding mental health"><input type="checkbox" id="anexiety" name="ch[]" value="understanding mental health" >understanding mental health</label>
-                <label for="understanding wellness"><input type="checkbox" id="anexiety" name="ch[]" value="understanding wellness" >understanding wellness</label>
-                <label for="victim support"><input type="checkbox" id="anexiety" name="ch[]" value="victim support" >victim support</label>
-               
-                
-            </form>
-            <div class="btns">
-                <button type="submit">NEXT</button> 
-                <button>pevious</button>
-                
-            </div>
-            
+    <form id="question-form" class ="wb-contents">
+  <div class="question">
+        <div class="step1">
+            <h2>We take care of our guests <br> MINDZOE is here for you , <br> whats are your problems ?</h2>
         </div>
+    <div class="formulaire">
+            <label for="anixiety"><input type="checkbox" id="anexiety" name="ch[]" value="anexity" >anexity</label>
+            <label for="Alcohol"><input type="checkbox" id="anexiety" name="ch[]" value="Alcohol" >Alcohol</label>
+            <label for="addiction and recovery"><input type="checkbox" id="anexiety" name="ch[]" value="addiction and recovery" >addiction and recovery</label>
+            <label for="depression"><input type="checkbox" id="anexiety" name="ch[]" value="depression" >depression</label>
+            <label for="drug use"><input type="checkbox" id="anexiety" name="ch[]" value="drug use" >drug use</label>
+            <label for="eating and body image"><input type="checkbox" id="anexiety" name="ch[]" value="eating and body image" >eating and body image</label>
+            <label for="grief and loss"><input type="checkbox" id="anexiety" name="ch[]" value="grief and loss" >grief and loss</label>
+            <label for="in crisis"><input type="checkbox" id="anexiety" name="ch[]" value="in crisis" >in crisis</label>
+            <label for="mental health care"><input type="checkbox" id="anexiety" name="ch[]" value="mental health care" >mental health care</label>
+            <label for="mental illness"><input type="checkbox" id="anexiety" name="ch[]" value="mental illness" >mental illness</label>
+            <label for="relationship and family violence"><input type="checkbox" id="anexiety" name="ch[]" value="relationship and family violence" >relationship and family violence</label>
+            <label for="stress"><input type="checkbox" id="anexiety" name="ch[]" value="stress" >stress</label>
+            <label for="support needs"><input type="checkbox" id="anexiety" name="ch[]" value="support needs" >support needs</label>
+            <label for="understanding mental health"><input type="checkbox" id="anexiety" name="ch[]" value="understanding mental health" >understanding mental health</label>
+            <label for="understanding wellness"><input type="checkbox" id="anexiety" name="ch[]" value="understanding wellness" >understanding wellness</label>
+            <label for="victim support"><input type="checkbox" id="anexiety" name="ch[]" value="victim support" >victim support</label>
+    </div>
+  </div>
+  
+  <div class="question">
+    <h3>Question 2</h3>
+    <p>Answer 2</p>
+    <label>
+      <input type="checkbox" name="answer2" value="Answer 2">
+      Answer 2
+    </label>
+  </div>
+  
+  <div class="question">
+    <h3>Question 3</h3>
+    <p>Answer 3</p>
+    <label>
+      <input type="checkbox" name="answer3" value="Answer 3">
+      Answer 3
+    </label>
+  </div>
+  <div class="btns">
+  <button type="submit" id="next-btn">Next</button>
+  </div>
+  
+</form>
+
+      
     </div>
 
     <script src="../assets/js/index.js"></script>
