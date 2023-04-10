95b50abb2491f8a653fecbbd7180844c0f3fb640
EyaNehdi <99126042+EyaNehdi@users.noreply.github.com>
2023-04-09 14:44:33 -1200

    crud without design

--- view/reclamation.php
+++ view/reclamation.php
@@ -1,4 +1,15 @@
 <?php 
-require "../controller/reclamationC.php";
-$reclam= new reclamC();
+$titre="reclamation";
 ?>
+<main>
+    <form action="" method="post">
+            <label>Id Reclamation <span color="red" >*</span></label>
+            <input type="text" minlength="4" maxlength="8" size="20" placeholder="Id de reclamation" required>
+            <label>Type de reclamation <span color="red" >*</span></label>
+            <input type="text" minlength="4" maxlength="8" size="20" placeholder="Type de reclamation" required>
+            <label>Description de réclamation <span >*</span></label>
+            <textarea  name="descriptionRec" rows="3" cols="20" placeholder="Veuillez écrire votre description" required></textarea>
+            <button class="buttonS" style="vertical-align:middle" type="submit">Submit</button>
+        <button type="reset">Reset</button>
+        </form>
+        </main>
