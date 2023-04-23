c2fe596197315baca5cd72144934c00dd2fad79c
ilyeschaabani <ilyeschaabani857@gmail.com>
2023-04-23 16:24:06 +0100

    post blog done

--- view/blogs.php
+++ view/blogs.php
@@ -1,6 +1,102 @@
 <?php
 $page_titre = "Connection";
 include './includes/header.php';
+require_once '../config.php'
+?>
+<?php
+$user_id = $_SESSION['id'];
+
+if(!isset($user_id))
+{
+   header('location:connection.php ');
+}
+
+if(isset($_POST['publish'])){
+
+  $name = $_POST['name'];
+  $name = filter_var($name, FILTER_SANITIZE_STRING);
+  $title = $_POST['title'];
+  $title = filter_var($title, FILTER_SANITIZE_STRING);
+  $content = $_POST['content'];
+  $content = filter_var($content, FILTER_SANITIZE_STRING);
+  $category = $_POST['category'];
+  $category = filter_var($category, FILTER_SANITIZE_STRING);
+  $status = 'active';
+  
+  $image = $_FILES['image']['name'];
+  $image = filter_var($image, FILTER_SANITIZE_STRING);
+  $image_size = $_FILES['image']['size'];
+  $image_tmp_name = $_FILES['image']['tmp_name'];
+  $image_folder = '../view/uploaded_img'.$image;
+
+  $select_image = $bdd->prepare("SELECT * FROM `posts` WHERE image = ? AND user_id = ?");
+  $select_image->execute([$image ,$user_id]);
+
+  if(isset($image)){
+     if($select_image->rowCount() > 0 AND $image != ''){
+        $message[] = 'image name repeated!';
+     }elseif($image_size > 2000000){
+        $message[] = 'images size is too large!';
+     }else{
+        move_uploaded_file($image_tmp_name, $image_folder);
+     }
+  }else{
+     $image = '';
+  }
+
+  if($select_image->rowCount() > 0 AND $image != ''){
+     $message[] = 'please rename your image!';
+  }else{
+     $insert_post = $bdd->prepare("INSERT INTO `posts`(user_id, name, title, content, category, image, status) VALUES(?,?,?,?,?,?,?)");
+     $insert_post->execute([$user_id, $name, $title, $content, $category, $image, $status]);
+     $message[] = 'post published!';
+  }
+  
+}
+
+if(isset($_POST['draft'])){
+
+  $name = $_POST['name'];
+  $name = filter_var($name, FILTER_SANITIZE_STRING);
+  $title = $_POST['title'];
+  $title = filter_var($title, FILTER_SANITIZE_STRING);
+  $content = $_POST['content'];
+  $content = filter_var($content, FILTER_SANITIZE_STRING);
+  $category = $_POST['category'];
+  $category = filter_var($category, FILTER_SANITIZE_STRING);
+  $status = 'deactive';
+  
+  $image = $_FILES['image']['name'];
+  $image = filter_var($image, FILTER_SANITIZE_STRING);
+  $image_size = $_FILES['image']['size'];
+  $image_tmp_name = $_FILES['image']['tmp_name'];
+  $image_folder = '../uploaded_img/'.$image;
+
+  $select_image = $conn->prepare("SELECT * FROM `posts` WHERE image = ? AND user_id = ?");
+  $select_image->execute([$image, $user_id]); 
+
+  if(isset($image)){
+     if($select_image->rowCount() > 0 AND $image != ''){
+        $message[] = 'image name repeated!';
+     }elseif($image_size > 2000000){
+        $message[] = 'images size is too large!';
+     }else{
+        move_uploaded_file($image_tmp_name, $image_folder);
+     }
+  }else{
+     $image = '';
+  }
+
+  if($select_image->rowCount() > 0 AND $image != ''){
+     $message[] = 'please rename your image!';
+  }else{
+     $insert_post = $conn->prepare("INSERT INTO `posts`(user_id, name, title, content, category, image, status) VALUES(?,?,?,?,?,?,?)");
+     $insert_post->execute([$user_id, $name, $title, $content, $category, $image, $status]);
+     $message[] = 'draft saved!';
+  }
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,67 +104,55 @@ include './includes/header.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="../assets/css/reclamation.css"> -->
+    <link rel="stylesheet" href="../assets/css/blogs.css">
     
     <title>BLOGS</title>
 </head>
 <body>
+<section class="post-editor">
 
+<h1 class="heading">ADD NEW POST</h1>
 
+<form action="" method="post" enctype="multipart/form-data">
+   <input type="hidden" name="name" value="<?= $fetch_profile['name']; ?>">
+   <p>post title <span>*</span></p>
+   <input type="text" name="title" maxlength="100" required placeholder="add post title" class="box">
+   <p>post content <span>*</span></p>
+   <textarea name="content" class="box" required maxlength="10000" placeholder="write your content..." cols="30" rows="10"></textarea>
+   <p>post category <span>*</span></p>
+   <select name="category" class="box" required>
+      <option value="" selected disabled>-- select category* </option>
+      <option value="nature">nature</option>
+      <option value="education">education</option>
+      <option value="pets and animals">pets and animals</option>
+      <option value="technology">technology</option>
+      <option value="fashion">fashion</option>
+      <option value="entertainment">entertainment</option>
+      <option value="movies and animations">movies</option>
+      <option value="gaming">gaming</option>
+      <option value="music">music</option>
+      <option value="sports">sports</option>
+      <option value="news">news</option>
+      <option value="travel">travel</option>
+      <option value="comedy">comedy</option>
+      <option value="design and development">design and development</option>
+      <option value="food and drinks">food and drinks</option>
+      <option value="lifestyle">lifestyle</option>
+      <option value="personal">personal</option>
+      <option value="health and fitness">health and fitness</option>
+      <option value="business">business</option>
+      <option value="shopping">shopping</option>
+      <option value="animations">animations</option>
+   </select>
+   <p>post image</p>
+   <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png, image/webp">
+   <div class="flex-btn">
+      <input type="submit" value="publish post" name="publish" class="btn">
+      <input type="submit" value="save draft" name="draft" class="btn">
+   </div>
+</form>
 
-
-    <div class="blog-container">
-        <div class="center">
-        <a href="./listeblogs.php">Back to list </a>
-            <form id="f" name="f" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post"  >
-                <div class="inputbox">
-                <input type="text" id="Idblog" name="Idblog" required="required">
-                <lAbel>ID blog</lAbel>
-                </div>
-                <div class="inputbox">
-                <input type="text" id="username" name="username" required="required">
-                <lAbel>username</lAbel>
-                </div>
-                <div class="inputbox">
-                    <textarea id="blogcentent" name="blogcentent" placeholder="blogcentent" ></textarea>
-                </div>
-                <div class="inputbox">
-                  <input type="text" id="titre" name="titre" required="required">
-                  <lAbel>titre</lAbel>
-                </div>
-                <div class="inputbox">
-                  <input type="datetime-local" id="dateblog" name="dateblog" required="required">
-                  <label>DATE</label>
-                </div>
-                <div class="inputbox">
-                  <input type="text" id="typeblog" name="typeblog" required="required">
-                  <span>typeblog</span>
-                </div>
-                
-                <div class="inputbox">
-                    <button type="submit"  >Submit</button> 
-                 <br>
-                 
-                    <button type="Reset">Reset</button>
-                  </div>
-    </div>
+</section>
     <!-- <script src="../assets/js/reclamation.js"></script> -->
 </body>
 </html>
-
-
-
-
-
-<?php
-require_once '../Controller/blogc.php';
-require_once '../Model/blog.php';
-$pc = new blogC();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $r = new blogm($_POST['Idblog'], $_POST['typeblog'], $_POST['blogcentent'], $_POST['titre'], $_POST['username'], $_POST['dateblog']);
-    
-        $pc->addBlog($r);
-       // $pc->addcrud($c);
-      }
-?>
